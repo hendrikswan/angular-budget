43b00bc0367233bdaa2b443d7ed4d8fe5b255d2e
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2013-11-25 22:12:51 +0200

    autocommit on change

--- app/scripts/directives/budgetList.js
+++ app/scripts/directives/budgetList.js
@@ -3,6 +3,7 @@
 angular.module('angBudgetApp')
   .directive('budgetList', function () {
     return {
+      scope: {},
       templateUrl: 'views/budgetList.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
